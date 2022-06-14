e5c6352b3ba10349a759675bb2f7a6ee623c16bb
Anderson Souza <61986093+andersonss3333@users.noreply.github.com>
2022-06-13 22:14:46 -0300

    Update and rename abstratcClassPegarDados.php to abstratcClassPegarDado.php

--- abstratcClassPegarDado.php
+++ abstratcClassPegarDado.php
@@ -1,14 +1,5 @@
 <?php
 
-interface pegarDados
-{
-
-	public function obterDados (string $url): string;
-	
-	//A função retornará uma closure, para ser usada  a callback necessaria para limitar o download dos dados.
-	public function dividirDadosClosure(): closure;
-}
-
 abstract class PegarDado implements pegarDados
 {
 	protected $url= null,
