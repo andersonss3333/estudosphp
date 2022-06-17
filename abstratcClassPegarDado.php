42e05ddaea5385f9cf02b78f60ad525601560b1d
Anderson Souza <61986093+andersonss3333@users.noreply.github.com>
2022-06-16 22:09:36 -0300

    Update abstratcClassPegarDado.php
    Finalizado o construtor da classe abstrata, e acrescida a propriedade privada e constante, com a expressão regular para validar a URL passada.

--- abstratcClassPegarDado.php
+++ abstratcClassPegarDado.php
@@ -8,23 +8,24 @@ abstract class PegarDado implements pegarDados
 	//Mantém o limite final pra o download do arquivo.
 	$ate= null;
 	
+	private const padrao= '/(https|http)://{1}w{3}\.{1}[a-z0-9]+\.{1}[a-z]{2,3}\.{0,1}[a-z]{0,2}/';
+	
 	private static $criarRecurso= null;
 
-    function __construct (string $url)
-    {
-    	if(empty ($url))
+        function __construct (string $url)
+        {
+    	     if(empty ($url))
     	{
     		//Por enquanto sem usar as exceptions.
     		echo 'Obrigatorio informar a URL';
     		
-    	} else
+    	} else if (preg_match_all(padrao, $url))
         {
         	$this->url= $url;
+        	
+        } else
+        {
+        	echo 'Obrigatorio informar uma URL correta';
         }
-    }
-	
-	public final function obterDados (string $url): string
-    {
-    	
     }
 }
