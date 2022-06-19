a0715038155862fac8f266c65dd2a9fe12152edd
Anderson Souza <61986093+andersonss3333@users.noreply.github.com>
2022-06-19 17:07:59 -0300

    Update abstratcClassPegarDado.php
    Modificado o valor da propriedade $ate, para zero(0), pois, ela será obrigatoriamente do tipo inteiro.
    E criada a variável que manterá os dados baixados.

--- abstratcClassPegarDado.php
+++ abstratcClassPegarDado.php
@@ -6,7 +6,9 @@ abstract class PegarDado implements pegarDados
 	//Mantem o ponto inial pra comecar a limitar o diwnload dos dados.
 	$comecaEm= 0,
 	//Mantém o limite final pra o download do arquivo.
-	$ate= null;
+	$ate= 0,
+        //Valor inicial pra os dados baixados
+        $dados= 0;
 	
 	private const padrao= '/^htt(ps|p)://w{3}[0-9]{0,8}\.[a-z0-9]+(:[0-9]{2,8})?\.[a-z]{2,3}(\.[a-z]{0,2})?(\/[a-z0-9]+\.[a-z]{2,8})?/';
 	
