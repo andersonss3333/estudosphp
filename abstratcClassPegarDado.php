3f72541a6ddf5bb17e02111658a7b407d3fdbc72
Anderson Souza <61986093+andersonss3333@users.noreply.github.com>
2022-06-27 12:40:21 -0300

    Update abstratcClassPegarDado.php
    Modificando a propriedade $bytes para null e acrescentando no construtor a atribuição do argumento $tamanhoDownload.

--- abstratcClassPegarDado.php
+++ abstratcClassPegarDado.php
@@ -6,11 +6,11 @@ abstract class PegarDado implements pegarDados
     //Mantem o ponto inial pra comecar a limitar o diwnload dos dados.
     $comecaEm= 0,
     //Limite de dados a serem baixados
-    $bytes= 1000;
+    $bytes= null;
   
     private const padrao= '<^htt(ps|p)://((www|[^w]{2}\.?m?\.?)?[0-9]{0,6}\.)?[a-z0-9]+(:[0-9]{2,8})?\.[a-z]{2,3}(\.[a-z]{0,2})?(\/[a-z0-9]+\.[a-z]{2,8})?>';
   
-    function __construct (string $url)
+    function __construct (string $url, ?int $tamanhoDownload= null)
     {
         if(empty ($url))
         {
@@ -20,6 +20,7 @@ abstract class PegarDado implements pegarDados
         } else if (preg_match_all(self::padrao, $url))
         {
           $this->url= $url;
+          $rhis->bytes= $tamanhoDownload;
           
         } else
         {
