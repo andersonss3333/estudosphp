82cd4aaa334d3446a430456abef42b5b7e79d780
Anderson Souza <61986093+andersonss3333@users.noreply.github.com>
2022-07-02 20:06:23 -0300

    Update abstratcClassPegarDado.php
    Colocando a declaração do strict_types no início do arquivo.

--- abstratcClassPegarDado.php
+++ abstratcClassPegarDado.php
@@ -1,10 +1,10 @@
 <?php
 
+declare(strict_types= 1);
+
 init_set('display_errors', 1);
 init_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-declare(strict_types= 1);
 
 abstract class PegarDado implements pegarDados
 {
